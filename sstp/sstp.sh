70448fc010fea109e2d501685a9b877c96784393
susantoyohanes <99692248+susantoyohanes@users.noreply.github.com>
2022-02-15 09:07:57 +0700

    Update sstp.sh

--- sstp/sstp.sh
+++ sstp/sstp.sh
@@ -26,7 +26,7 @@ echo -e "${NC}${LIGHT}Telegram : https://t.me/susantoyohanes69"
 exit 0
 fi
 # Link Hosting Kalian
-akbarvpn="https://raw.githubusercontent.com/susantoyohanes/scku/main/sstp"
+akbarvpn="raw.githubusercontent.com/susantoyohanes/scku/main/sstp"
 
 MYIP=$(wget -qO- ipinfo.io/ip);
 MYIP2="s/xxxxxxxxx/$MYIP/g";
