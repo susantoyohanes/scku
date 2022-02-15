d61638c026cc8e36653a2437a2dac0f8688b8a9b
susantoyohanes <99692248+susantoyohanes@users.noreply.github.com>
2022-02-15 08:57:33 +0700

    Update set-br.sh

--- backup/set-br.sh
+++ backup/set-br.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 # Link Hosting Kalian
-akbarvpn="https://raw.githubusercontent.com/susantoyohanes/scku/main/backup"
+akbarvpn="raw.githubusercontent.com/susantoyohanes/scku/main/backup"
 
 apt install rclone -y
 printf "q\n" | rclone config
